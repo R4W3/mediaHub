403d986ced808d21a4fed3a82393025f63af6931
R4W3 <rene.ehlers199902@gmail.com>
2021-02-04 21:17:35 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template
 from flask_compress import Compress
-import mysql.connector
-import requests
+from music_lib import music
 
 app = Flask(__name__)
 app.secret_key = "dfsgjl646385sädjkgjhhöalosdhhtas"
 Compress(app)
 
+
 @app.route("/")
 def homepage():
+    m_lib = music
 
-    return render_template("/index/index.html")
+    return render_template("/index/index.html", m_lib=m_lib)
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5001, debug=True)
---
+++ music_lib.py
@@ -0,0 +1,4 @@
+music = {
+    1: {"artist": "Taylor Swift", "album": "Fearless", "path": "/static/media/music/Taylor Swift - Fearless",
+        "cover": "http://localhost:5001/static/media/music/Taylor Swift - Fearless/cover.jpg"}
+}
